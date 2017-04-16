9de489bd2c3e51adbe7c39dd0bda95466ef824e5
Samara Somoza <sammysomoza@hotmail.com>
2017-04-16 19:26:29 -0400

    Fingers Crossed Part 2

--- web.py
+++ web.py
@@ -17,7 +17,7 @@ def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
    
